6bdf2512a901547f91c61b4c285b185e8336ab3d
Maximus Harding <maximusharding@gmail.com>
2023-03-19 17:53:46 -0500

    Moved GraphQL queries to their respective functions.

--- SCP Utility/Search/Crom.swift
+++ SCP Utility/Search/Crom.swift
@@ -9,36 +9,6 @@ import Foundation
 import SwiftyJSON
 import Alamofire
 
-func CromQueryFromUrl(query: String) -> String {
-return """
-query Search($query: URL! = "\(query)") {
-    page(url: $query) {
-    wikidotInfo {
-      title
-      source
-      thumbnailUrl
-    }
-  }
-}
-"""
-}
-
-func CromQuery(query: String) -> String {
-return """
-query Search($query: String! = "\(query)") {
-  searchPages(query: $query, filter: {anyBaseUrl: "http://scp-wiki.wikidot.com"}) {
-    wikidotInfo {
-      title
-      source
-      thumbnailUrl
-    }
-  }
-}
-"""
-}
-
-
-
 // https://api.crom.avn.sh/graphql
 func cromRequest(params: [String:String], completion: @escaping (Data?, Error?) -> Void) {
     let headers: HTTPHeaders = [
@@ -46,7 +16,6 @@ func cromRequest(params: [String:String], completion: @escaping (Data?, Error?) 
     ]
     
     let url = "https://api.crom.avn.sh/graphql"
-    
 
     AF.request(url, method: .post, parameters: params, encoding: JSONEncoding.default, headers: headers).responseData { response in
         switch response.result {
@@ -58,61 +27,71 @@ func cromRequest(params: [String:String], completion: @escaping (Data?, Error?) 
     }
 }
 
-func cromURLSearch(query: String) -> Article {
-    var dataResponse = Data()
-
-    let headers: HTTPHeaders = [
-        "Content-Type": "application/json"
-    ]
+var dataResponse = Data()
+func cromAPISearchFromURL(query: String, completion: @escaping (Article) -> Void) {
+    let graphQLQuery = """
+query Search($query: URL! = "\(query)") {
+    page(url: $query) {
+    wikidotInfo {
+      title
+      source
+      thumbnailUrl
+    }
+  }
+}
+"""
     
-    let queryhttp = "http" + query.dropFirst(5) // The query has to be http
     let parameters: [String: String] = [
-        "query": (CromQueryFromUrl(query: queryhttp))
+        "query": (graphQLQuery)
     ]
 
-    let url = "https://api.crom.avn.sh/graphql"
     var responseJSON: JSON = JSON()
     
-    AF.request(url, method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: headers).responseData { response in
-        switch response.result {
-        case .success(let data):
-            dataResponse = data
-        case .failure(let error):
-            print("Error: \(error)")
+    var article = Article(title: "", pagesource: "")
+    cromRequest(params: parameters) { data, error in
+        if let error {
+            print(error)
+        } else if let myData = data {
+            do {
+                responseJSON = try JSON(data: myData)
+            } catch {
+                print(error)
+            }
+
+            for pages in responseJSON["data"]["searchPages"].arrayValue {
+                let title = pages["wikidotInfo"]["title"]
+                let source = pages["wikidotInfo"]["source"]
+                let pic = pages["wikidotInfo"]["thumbnailUrl"]
+
+                article = Article(
+                    title: title.string ?? "Could not find title",
+                    pagesource: source.string ?? "Could not find pagesource",
+                    thumbnail: pic.url ?? nil
+                )
+            }
+            completion(article)
         }
     }
-    
-    var articles: Article
-    do {
-        responseJSON = try JSON(data: dataResponse)
-    } catch {
-        print(error)
-    }
-
-    let title = responseJSON["data"]["page"]["wikidotInfo"]["title"]
-    let source = responseJSON["data"]["page"]["wikidotInfo"]["source"]
-    let pic = responseJSON["data"]["page"]["wikidotInfo"]["thumbnailUrl"]
-
-    articles = Article(
-        title: title.string ?? "Could not find title",
-        pagesource: source.string ?? "Could not find pagesource",
-        thumbnail: pic.url ?? nil
-    )
-        
-
-    return articles
-    
 }
 
-var dataResponse = Data()
 func cromAPISearch(query: String, completion: @escaping ([Article]) -> Void) {
+    let graphQLQuery = """
+query Search($query: String! = "\(query)") {
+  searchPages(query: $query, filter: {anyBaseUrl: "http://scp-wiki.wikidot.com"}) {
+    wikidotInfo {
+      title
+      source
+      thumbnailUrl
+    }
+  }
+}
+"""
+    
     let parameters: [String: String] = [
-        "query": (CromQuery(query: query))
+        "query": (graphQLQuery)
     ]
 
-    let url = "https://api.crom.avn.sh/graphql"
     var responseJSON: JSON = JSON()
-    
     
     var articles: [Article] = []
     cromRequest(params: parameters) { data, error in
