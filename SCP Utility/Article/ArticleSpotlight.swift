e326f1966866fc58c5e9373302b345778e6fd98b
Maximus Harding <maximusharding@gmail.com>
2023-03-25 18:53:56 -0500

    Added confirmation on settings buttons

--- SCP Utility/Article/ArticleSpotlight.swift
+++ SCP Utility/Article/ArticleSpotlight.swift
@@ -11,49 +11,59 @@ import Kingfisher
 struct ArticleSpotlight: View {
     @State var scp: Article
     var body: some View {
-        VStack {
-            HStack {
-                Text(scp.title)
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .padding(.leading)
-                Spacer()
+        NavigationLink(destination: ArticleView(scp: scp)) {
+            VStack {
+                HStack {
+                    Text(scp.title)
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .padding(.leading)
+                    Spacer()
+                }
+                HStack {
+                    Spacer()
+                    ZStack{
+                        if scp.objclass != nil {
+                            Image(scp.objclass!.toImage())
+                        }
+                    }
+                    
+                    ZStack{
+                        if scp.esoteric != nil {
+                            Image(scp.esoteric!.toImage())
+                        }
+                    }
+                    
+                    ZStack{
+                        if scp.disruption != nil {
+                            Image(scp.disruption!.toImage())
+                        }
+                    }
+                    
+                    ZStack{
+                        if scp.risk != nil {
+                            Image(scp.risk!.toImage())
+                        }
+                    }
+                    Spacer()
+                }
             }
-            HStack {
-                Spacer()
-                ZStack{
-                    if scp.objclass != nil { Image(scp.objclass!.toImage()) }
+            .padding(10)
+            .cornerRadius(15)
+            .background {
+                if scp.thumbnail != nil {
+                    KFImage(scp.thumbnail!)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .opacity(0.5)
+                    // https://stackoverflow.com/a/69092800
+                        .mask(LinearGradient(gradient: Gradient(stops: [
+                            .init(color: .black, location: 0),
+                            .init(color: .clear, location: 1),
+                            .init(color: .black, location: 1),
+                            .init(color: .clear, location: 1)
+                        ]), startPoint: .top, endPoint: .bottom))
                 }
-
-                ZStack{
-                    if scp.esoteric != nil { Image(scp.esoteric!.toImage()) }
-                }
-
-                ZStack{
-                    if scp.disruption != nil { Image(scp.disruption!.toImage()) }
-                }
-
-                ZStack{
-                    if scp.risk != nil { Image(scp.risk!.toImage()) }
-                }
-                Spacer()
-            }
-        }
-        .padding(10)
-        .cornerRadius(15)
-        .background {
-            if scp.thumbnail != nil {
-                KFImage(scp.thumbnail!)
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .opacity(0.5)
-                // https://stackoverflow.com/a/69092800
-                    .mask(LinearGradient(gradient: Gradient(stops: [
-                                .init(color: .black, location: 0),
-                                .init(color: .clear, location: 1),
-                                .init(color: .black, location: 1),
-                                .init(color: .clear, location: 1)
-                            ]), startPoint: .top, endPoint: .bottom))
             }
         }
     }
@@ -63,7 +73,7 @@ struct ArticleSpotlight_Previews: PreviewProvider {
     static var previews: some View {
         ArticleSpotlight(scp: Article(
             title: "SCP-5004",
-            pagesource: "",
+            pagesource: "Page source here...",
             thumbnail: URL(string: "https://scp-wiki.wdfiles.com/local--files/scp-5004/header.png"),
             objclass: .esoteric,
             esoteric: .thaumiel,
--- SCP Utility/Settings/SettingsView.swift
+++ SCP Utility/Settings/SettingsView.swift
@@ -12,6 +12,11 @@ struct SettingsView: View {
     @AppStorage("articleViewSetting") var articleViewSetting = 0
     @AppStorage("showImages") var showImages = true
     
+    @State var historyConf = false
+    @State var listConf = false
+    @State var articleConf = false
+    @State var allDataConf = false
+
     let con = PersistenceController.shared
     var body: some View {
         Form {
@@ -26,13 +31,38 @@ struct SettingsView: View {
             
             Section(header: Text("HISTORY_OPTIONS")) {
                 Toggle("TRACK_HISTORY", isOn: $trackHistory)
-                Button("DELETE_ALL_HISTORY") { con.deleteAllHistory() }
+                Button("DELETE_ALL_HISTORY") {
+                    historyConf = true
+                }.confirmationDialog("ASSURANCE", isPresented: $historyConf) {
+                    Button("ASSURANCE", role: .destructive) {
+                        con.deleteAllHistory()
+                    }
+                }
             }
             
             Section(header: Text("DATA_OPTIONS")) {
-                Button("DELETE_ALL_LISTS") { con.deleteAllLists() }
-                Button("DELETE_ALL_ARTICLES") { con.deleteAllArticles() }
-                Button("DELETE_ALL_DATA") { con.deleteAllData() }
+                Button("DELETE_ALL_LISTS") {
+                    listConf = true
+                }.confirmationDialog("DELETE_LIST_TOOLTIP", isPresented: $listConf, titleVisibility: .visible) {
+                    Button("DELETE", role: .destructive) {
+                        con.deleteAllLists()
+                    }
+                }
+                Button("DELETE_ALL_ARTICLES") {
+                    articleConf = true
+                }.confirmationDialog("ASSURANCE", isPresented: $articleConf) {
+                    Button("ASSURANCE", role: .destructive) {
+                        con.deleteAllLists()
+                    }
+                }
+                   
+                Button("DELETE_ALL_DATA") {
+                    allDataConf = true
+                }.confirmationDialog("ASSURANCE", isPresented: $allDataConf) {
+                    Button("ASSURANCE", role: .destructive) {
+                        con.deleteAllData()
+                    }
+                }
             }
         }
         .navigationTitle("SETTINGS_TITLE")
