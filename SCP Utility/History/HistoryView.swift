49902062511be1955dfc45e1152ff6b6bdfa8d6d
Maximus Harding <maximusharding@gmail.com>
2023-06-11 16:19:01 -0500

    added categorization to history view

--- SCP Utility/History/HistoryView.swift
+++ SCP Utility/History/HistoryView.swift
@@ -6,32 +6,66 @@
 //
 
 import SwiftUI
-import Foundation
 
 /// View that displays multiple HistoryRows, and adds deletion of history items.
 struct HistoryView: View {
-    @State var clearConfirmation: Bool = false
+    @State private var clearConfirmation: Bool = false
     @State private var items: [HistoryItem]? = nil
     @State private var query: String =  ""
     
     let con = PersistenceController.shared
+    
+    var timeIntervals: [Double] = [
+         -86400, // day
+         -604800, // week
+         -2628000, // month
+         -Double.infinity,
+    ]
+    
     var body: some View {
         NavigationStack {
             ScrollView {
                 if items != nil && items != [] {
-                    LazyVStack {
-                        ForEach(items!) { item in
-                            let history = History(fromEntity: item)!
-                            HistoryRow(item: history)
-                                .contextMenu {
-                                    Button(role: .destructive) {
-                                        con.deleteHistoryFromId(id: history.id)
-                                    } label: {
-                                        Label("DELETE", systemImage: "trash")
-                                    }
-                                }
-                            Divider()
+                    ForEach(Array(zip(timeIntervals, timeIntervals.indices)), id: \.1) { time, index in
+                        let lastTime = time == timeIntervals[0] ? 0 : timeIntervals[index - 1]
+                        let filteredItems = items!.filter {
+                            // Item is younger than interval
+                            ($0.date!.timeIntervalSinceNow > time) &&
+                            // Item is older than last interval
+                            ($0.date!.timeIntervalSinceNow < lastTime)
                         }
+                        
+                        if !filteredItems.isEmpty {
+                            HStack {
+                                switch index {
+                                case 0: Text("TODAY")
+                                case 1: Text("THIS_WEEK")
+                                case 2: Text("THIS_MONTH")
+                                default: Text("OLDER")
+                                }
+                                
+                                Spacer()
+                            }
+                            .font(.title2)
+                            .bold()
+                            .padding(.vertical, 3)
+                        }
+                        
+                        LazyVStack {
+                            ForEach(filteredItems) { item in
+                                let history = History(fromEntity: item)!
+                                HistoryRow(item: history)
+                                    .contextMenu {
+                                        Button(role: .destructive) {
+                                            con.deleteHistoryFromId(id: history.id)
+                                        } label: {
+                                            Label("DELETE", systemImage: "trash")
+                                        }
+                                    }
+                                Divider()
+                            }
+                        }
+                        .padding(.horizontal, 10)
                     }
                     .padding(.horizontal, 15)
                 } else {
