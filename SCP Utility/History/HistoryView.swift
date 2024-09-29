0e782aa56c1db5a3cf715fb939300ac57fc82f6d
Maximus Harding <maximusharding@gmail.com>
2024-09-29 14:17:51 -0500

    added date filtering to history view

--- SCP Utility/History/HistoryView.swift
+++ SCP Utility/History/HistoryView.swift
@@ -7,11 +7,15 @@
 
 import SwiftUI
 
-/// View that displays multiple HistoryRows, and adds deletion of history items.
+/// View that displays user article history, and allows filtering by time.
 struct HistoryView: View {
     @State private var clearConfirmation: Bool = false
     @State private var items: [HistoryItem]? = nil
     @State private var query: String =  ""
+    @State private var sheetPresent: Bool = false
+    @State private var rangeSpecified: Bool = false
+    @State private var startDate: Date = Date().onlyDate
+    @State private var endDate: Date = Date().onlyDate
     
     let con = PersistenceController.shared
     
@@ -25,6 +29,22 @@ struct HistoryView: View {
     var body: some View {
         NavigationStack {
             ScrollView {
+                if rangeSpecified {
+                    HStack(alignment: .center) {
+                        Button {
+                            startDate = Date().onlyDate
+                            endDate = Date().onlyDate
+                            rangeSpecified = false
+                            updateList()
+                        } label: {
+                            Text("RESET_FILTER")
+                                .font(.title3)
+                                .bold()
+                                .padding(.vertical, 3)
+                        }
+                    }
+                }
+                
                 if items != nil && items != [] {
                     ForEach(Array(zip(timeIntervals, timeIntervals.indices)), id: \.1) { time, index in
                         let lastTime = time == timeIntervals[0] ? 0 : timeIntervals[index - 1]
@@ -35,7 +55,7 @@ struct HistoryView: View {
                             ($0.date!.timeIntervalSinceNow < lastTime)
                         }
                         
-                        if !filteredItems.isEmpty {
+                        if !filteredItems.isEmpty && !rangeSpecified {
                             HStack {
                                 switch index {
                                 case 0: Text("TODAY")
@@ -75,21 +95,60 @@ struct HistoryView: View {
             }
             .navigationTitle("HISTORY_TITLE")
             .toolbar {
-                Button {
-                    clearConfirmation = true
+                Menu {
+                    Button {
+                        sheetPresent = true
+                    } label: {
+                        Label("SELECT_DATE", systemImage: "calendar")
+                    }
+                    
+                    Button {
+                        clearConfirmation = true
+                    } label: {
+                        Label("CLEAR_HISTORY_BUTTON", systemImage: "trash")
+                    }
+                    .confirmationDialog("ASSURANCE", isPresented: $clearConfirmation) {
+                        Button("CLEAR_HISTORY_CONFIRMATION", role: .destructive) {
+                            con.deleteAllHistory()
+                            updateList()
+                        }
+                    }
                 } label: {
-                    Label("CLEAR_HISTORY_BUTTON", systemImage: "trash")
+                    Image(systemName: "ellipsis.circle")
                 }
-                .confirmationDialog("ASSURANCE", isPresented: $clearConfirmation) {
-                    Button("CLEAR_HISTORY_CONFIRMATION", role: .destructive) {
-                        con.deleteAllHistory()
+            }
+            .sheet(isPresented: $sheetPresent) {
+                VStack {
+                    Text("SELECT_DATE")
+                        .font(.title)
+                        .bold()
+                    DatePicker("START_DATE", selection: $startDate, in: ...Date(), displayedComponents: [.date])
+                    DatePicker("END_DATE", selection: $endDate, in: ...Date(), displayedComponents: [.date])
+                    
+                    Button {
+                        sheetPresent.toggle()
+                        rangeSpecified = true
+                    } label: {
+                        Text("DONE")
+                    }
+                    .padding(.vertical, 10)
+                    
+                    Button {
+                        sheetPresent.toggle()
+                        rangeSpecified = false
                         updateList()
+                    } label: {
+                        Text("CANCEL")
                     }
                 }
+                .padding(.horizontal, 20)
+                .onDisappear { updateList() }
+                .presentationDetents([.medium])
             }
         }
         .searchable(text: $query)
         .onChange(of: query) { _ in
+            rangeSpecified = false
             updateList()
             if query == "" {
                 items = items?.reversed()
@@ -105,6 +164,16 @@ struct HistoryView: View {
     private func updateList() {
         items = con.getAllHistory()?.reversed()
         items?.sort { ($0.date ?? Date()) > ($1.date ?? Date()) }
+        
+        if rangeSpecified {
+            if startDate > endDate { swap(&startDate, &endDate) }
+            // Assume user wanted to see all values on a day if dates are equal
+            if startDate == endDate { endDate += 86400 }
+            
+            items = (items ?? []).filter {
+                $0.date! < endDate && $0.date! > startDate
+            }
+        }
     }
 }
 
@@ -112,4 +181,18 @@ struct HistoryView_Previews: PreviewProvider {
     static var previews: some View {
         HistoryView()
     }
+}
+
+extension Date {
+    // https://stackoverflow.com/q/55688517/25654194
+    /// Returns the date up to the day.
+    var onlyDate: Date {
+        get {
+            let calendar = Calendar.current
+            var dateComponents = calendar.dateComponents([.year, .month, .day], from: self)
+            dateComponents.timeZone = NSTimeZone.system
+            return calendar.date(from: dateComponents) ?? self
+        }
+    }
+
 }
